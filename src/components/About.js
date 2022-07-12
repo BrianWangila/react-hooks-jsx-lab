3582ed6cc153bba231dd69bffe735eb63f0b8095
Brian Wangila <bwanjalah@gmail.com>
2022-07-12 22:13:13 +0300

    commit tree 83dc280c76c4702daa94c906864b1b28153c9f58

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { image } from "../data/data";
+import {name, city} from "../data/data"
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>This is a trial JSX by {name} from {city}</p>
+    <img src={image} alt = "I made this"/> 
+  </div>
+  );
 }
 
 export default About;
